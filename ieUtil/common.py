09d3f4c620a07855ba7cadda3fd3e608bfa3e963
ingenuity employee <gitWorker@ievfx.com>
2014-06-06 18:52:28 -0700

    half done w/ updates

--- ieUtil/common.py
+++ ieUtil/common.py
@@ -4,7 +4,6 @@ import time
 import random
 import hashlib
 import json
-from itertools import izip
 
 # only need to do this once
 random.seed(time.time())
@@ -15,24 +14,22 @@ REPLACEVARS = {
 '#92;':'\\'
 }
 
+
+'''
+	Method:  pad
+
+	Pads a number, <num> with zeros so the resulting string is <padding> digits long.
+'''
 def pad(num,padding):
 	num = str(num)
 	return '0'*(padding-len(num)) + num
 
-# currently only used in shepherd/JobTypes
-def checkNumParam(param,default):
-	if param == '':
-		return default
-	try:
-		param = defaultStringReplace(str(int(param)))
-	except:
-		param = str(default)
-	return param
 
-def listToDict(val):
-	i = iter(val)
-	return dict(izip(i, i))
+'''
+	Method: varType
 
+	Returns variable type of value passed in.
+'''
 def varType(val):
 	typeString = str(type(val))
 	variableType = re.search("'([a-z]+)'",typeString)
@@ -40,21 +37,21 @@ def varType(val):
 		return variableType.group(1)
 	return typeString
 
-def defaultStringReplace(str):
-	try:
-		for k,v in REPLACEVARS.items():
-			str = str.replace(k,v)
-	except:
-		pass
-	return str
+# def defaultStringReplace(str):
+# 	try:
+# 		for k,v in REPLACEVARS.items():
+# 			str = str.replace(k,v)
+# 	except:
+# 		pass
+# 	return str
 
-def defaultStringInsert(str):
-	try:
-		for k,v in REPLACEVARS.items():
-			str = str.replace(v,k)
-	except:
-		pass
-	return str
+# def defaultStringInsert(str):
+# 	try:
+# 		for k,v in REPLACEVARS.items():
+# 			str = str.replace(v,k)
+# 	except:
+# 		pass
+# 	return str
 
 # fix: breaks on single dash arguments, improve
 def getArgs(args=None):
@@ -100,20 +97,45 @@ def dictToAndFrom(val):
 def mergeDict(a,b):
 	return dict(a.items() + b.items())
 
+'''
+	Method: movieSafeDim
+
+	Rounds number DOWN to the nearest multiple of 4.
+'''
 def movieSafeDim(dim):
 	return int(int(dim) * .25) * 4;
 
+'''
+	Method: safeFilename
+
+	Removes unsafe characters from file names.
+'''
 def safeFilename(filename):
 	"""Return a filename with only file safe characters"""
 	validChars = '-_.()abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 	return ''.join(c for c in filename if c in validChars)
 
+'''
+	Method utcNow
+
+	Returns current UTC time.
+'''
 def utcNow():
 	return int(time.time() + time.timezone)
 
+'''
+	Method: randomHash
+
+	Returns random sha224 hash of given length.  Defaults to 16.
+'''
 def randomHash(length=16):
 	return hashlib.sha224(str(random.random())).hexdigest()[:length]
 
+'''
+	Method: makeArrayUnique
+
+	Returns given array with all unique elements.
+'''
 def makeArrayUnique(val, transformFunc=None):
 	def makeUnique(val, transformFunc):
 		seen = set()
@@ -132,3 +154,103 @@ def makeArrayUnique(val, transformFunc=None):
 				yield x
 	return list(makeUnique(val, transformFunc))
 
+'''
+	Method: makeWebSafe
+
+	Takes a string and converts all non-alphanumeric characters to underscores.
+	Makes all characters lowercase.
+'''
+def makeWebSafe(string):
+	return ''.join([i if i.isalpha() or i.isdigit() else '_' for i in string]).lower()
+
+'''
+	Method: getExtensions
+
+	Returns file extension all lowercase with no whitespace, preceded by a period.
+'''
+def getExtension(filename):
+	if '.' not in filename:
+		return ''
+	return '.' + filename.split('.')[-1].lower().strip()
+
+'''
+	Method: normalizeExtension
+
+	Returns file extension all lowercase with no whitespace, preceded by a period.
+'''
+def normalizeExtension(extension):
+	extension = extension.lower().strip()
+	if (extension[0] != '.'):
+		return '.' + extension
+	return extension
+
+'''
+	Method: removeExtension
+
+	Removes extension from filename.
+'''
+def removeExtension(filename):
+	if ('.' not in filename):
+		return filename
+	return '.'.join(filename.split('.')[:-1])
+
+'''
+	Method: ensureExtension
+
+	Checks that a given file has the given extension.  If not, appends the extension.
+'''
+def ensureExtension(filename, extension):
+	extension = normalizeExtension(extension)
+	if (getExtension(filename) != extension):
+		return filename + extension
+	return filename
+
+'''
+	Method: parseCommaArray
+
+	Turns 'likes, comments' into ['like', 'comments']
+'''
+def parseCommaArray(val):
+	return [i.strip() for i in val.split(',')]
+
+'''
+	Method: appendOrSetArray
+
+	Appends val to obj if obj is an array.
+	If obj is not, and val is not null, returns val as an array.
+	If val is null, returns [].
+'''
+def appendOrSetArray(obj, val):
+	if (isinstance(obj, list)):
+		obj.append(val)
+		return obj
+	if (isinstance(val, list)):
+		return val
+	if (val != None):
+		return [val]
+	return []
+
+'''
+	Method: ensureArray
+
+	Returns val as an array.
+	If val is null, returns [].
+'''
+def ensureArray(val):
+	if (isinstance(val, list)):
+		return val
+	if (val == None):
+		return []
+	return [val]
+
+'''
+	Method: ensureNumber
+
+	Returns float version of val if val is number or a string representing a number.
+	Otherwise returns 0.
+'''
+def ensureNumber(val):
+	try:
+		return float(val)
+	except:
+		return 0
